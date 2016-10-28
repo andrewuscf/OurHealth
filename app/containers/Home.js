cdbabaa8d68242fa68a960bcf02a6ee893f6f884
Andrew <andrewuscf@hotmail.com>
2016-10-27 23:54:59 -0700

    Added background geolocation services.

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -27,39 +27,46 @@ const Home = React.createClass({
     },
 
     componentDidMount() {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({position: position});
-            },
-            (error) => console.log(JSON.stringify(error)),
-            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-        );
-
-        this.watchID = navigator.geolocation.watchPosition((position) => {
-            this.setState({position: position});
-        });
+    //     navigator.geolocation.getCurrentPosition(
+    //         (position) => {
+    //             this.setState({position: position});
+    //         },
+    //         (error) => console.log(JSON.stringify(error)),
+    //         {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    //     );
+    //
+    //     this.watchID = navigator.geolocation.watchPosition((position) => {
+    //         this.setState({position: position});
+    //     });
     },
 
-    getUsers() {
+    getUsers(refresh = false) {
         // Get workers if current user is client and vice verus.
-        if (this.props.RequestUser.profile.type == 'Client') {
-            this.props.actions.loadWorkers(this.state.position);
+        if (this.props.RequestUser.type == 'Client') {
+            this.props.actions.loadWorkers(this.state.position, refresh);
         } else {
             // this.props.actions.loadClients();
         }
     },
 
     componentDidUpdate(prevProps, prevState) {
-        if (this.state.position != prevState.position && this.props.RequestUser) {
+        // Initial with position
+        if (!prevState.position  && this.state.position && this.props.RequestUser) {
             this.getUsers();
         }
-        if (this.props.RequestUser != prevProps.RequestUser && this.state.position) {
-            this.getUsers();
-        }
+        // Get when user location changes.
+        // if (this.props.RequestUser != prevProps.RequestUser && this.state.position) {
+        //     console.log('hit');
+        //     this.getUsers();
+        // }
     },
 
     refresh() {
-        this.props.actions.loadWorkers(this.state.position, true);
+        getUsers(true);
+    },
+
+    onEndReached() {
+        console.log('hit')
     },
 
     _redirect(routeName, props = null) {
