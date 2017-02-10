7cc22b6ea0bfdbf449db42ecda210644d8e0c6eb
andrewuscf <andrewuscf@hotmail.com>
2017-02-09 17:48:34 -0800

    Styling empty calendar.

--- app/containers/Calendar.js
+++ app/containers/Calendar.js
@@ -1,10 +1,11 @@
 'use strict';
 
 import React from 'react';
-import {StyleSheet, Text, View, ListView, Platform} from 'react-native';
+import {StyleSheet, Text, View, ListView, Platform, ScrollView, RefreshControl} from 'react-native';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import moment from 'moment';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import * as HomeActions from '../actions/HomeActions';
 
@@ -54,20 +55,38 @@ const Calendar = React.createClass({
         this.props.openModal();
     },
 
+    _refresh() {
+        // this.props.actions.getChatRooms(true);
+    },
+
     render() {
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        const dataSource = ds.cloneWithRows(this.state.schedule);
-        return (
-            <View style={styles.mainContainer}>
-                <Text>Calendar</Text>
-                <ListView removeClippedSubviews={(Platform.OS === 'ios') ? false : true}
-                          enableEmptySections={true}
-                          dataSource={dataSource}
-                          renderRow={(day, i) => <DayBox key={i} day={day} RequestUser={this.props.RequestUser}
+        if (this.state.schedule.length) {
+            const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+            const dataSource = ds.cloneWithRows(this.state.schedule);
+            return (
+                <View style={styles.mainContainer}>
+                    <Text>Calendar</Text>
+                    <ListView removeClippedSubviews={(Platform.OS === 'ios') ? false : true}
+                              enableEmptySections={true}
+                              dataSource={dataSource}
+                              renderRow={(day, i) => <DayBox key={i} day={day} RequestUser={this.props.RequestUser}
                                                          checkInModal={this.checkInModal}
                                                          index={i}/>}
-                />
-            </View>
+                    />
+                </View>
+            );
+        }
+        return (
+            <ScrollView contentContainerStyle={styles.scrollContainer}
+                        refreshControl={<RefreshControl refreshing={this.props.Refreshing} onRefresh={this._refresh}/>}>
+                <View style={styles.noRequests}>
+                    <Icon name="calendar-check-o" size={60}
+                          color='#b1aea5'/>
+                    <Text style={styles.noRequestTitle}>
+                        You do not have an upcoming work.
+                    </Text>
+                </View>
+            </ScrollView>
         );
     }
 });
@@ -77,6 +96,24 @@ const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
     },
+    scrollContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    noRequests: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 20
+    },
+    noRequestTitle: {
+        fontSize: 15,
+        color: '#b1aeb9',
+        textAlign: 'center',
+        paddingTop: 20,
+        // fontFamily: 'OpenSans-Semibold'
+    }
 });
 
 const stateToProps = (state) => {
